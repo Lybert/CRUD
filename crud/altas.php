2e9be73e66d10655d38a0bb74e6859b26576d6d6
xLybert <felixgs2@hotmail.com>
2016-01-09 14:06:34 +0100

    Primeros pasos

---
+++ crud/altas.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Añadir Cliente</title>
+    </head>
+    <body>
+        <!-- el código php vendrá aquí -->
+    </body>
+</html>
---
+++ crud/config-default.php
@@ -0,0 +1,6 @@
+<?php
+// parámetros usado para conectar a la base de datos
+$host = "172.20.100.21";
+$db_name = "melol";
+$username = "ausias";
+$password = "ausias";
